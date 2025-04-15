aa044308894d6def42a1b4a118e3eff1ecd32da2
BeaRozz <asucamedina@gmail.com>
2025-04-14 18:59:11 -0600

    projects-base

---
+++ proyecto-react/src/page/About.js
@@ -0,0 +1,10 @@
+import '../styles/home.css'
+
+export default function About() {
+  
+  return (
+    <div className="base">
+      <h1>Acerca de</h1>
+    </div>
+  );
+}
---
+++ proyecto-react/src/page/Home.js
@@ -0,0 +1,9 @@
+import '../styles/home.css'
+
+export default function Home() {
+    return (
+      <div className="base">
+        <h1>Home</h1>
+      </div>
+    );
+}
